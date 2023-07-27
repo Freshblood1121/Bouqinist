9663e1393e1b4431d24683465ff99a9229369026
FreshBlood1121 <perviburger@gmail.com>
2023-07-27 17:11:54 +0300

    Add migrates and models for Books,Categories tables.

---
+++ database/migrations/2023_07_27_133607_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+            $table->integer('age');
+            $table->string('author', 255);
+            $table->text('publishing');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
