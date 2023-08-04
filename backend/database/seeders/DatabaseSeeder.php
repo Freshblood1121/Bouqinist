36033042aae56f5e71469fe586427fceba146499
Bor333 <mugen999999999@gmail.com>
2023-08-04 17:26:40 +0700

    seeder fix

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(
-            // BookSeeder::class,
-           CategorySeeder::class,
+            CategorySeeder::class,
+            BookSeeder::class,
         );
     }
 }
