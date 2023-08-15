306a23a6389d4f7b8ac3d1369e5162e98b12ba2b
Artem <forzeX1@gmail.com>
2023-08-15 17:41:24 +0300

    hotfix: sliders for sections "popular" and "catalog"

--- frontend/src/Components/MainPage/Popular/Popular.jsx
+++ frontend/src/Components/MainPage/Popular/Popular.jsx
@@ -1,104 +1,124 @@
 import React, { useRef, useState } from "react";
 import "./Popular.css";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
+// import "swiper/css";
+
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css/core";
 
 const Popular = () => {
   const swiperRef = useRef();
+
+  const popularItems = [
+    { img: "./img/popular/cb1_1.jpg" },
+    { img: "./img/popular/cb1_2.jpg" },
+    { img: "./img/popular/cb1_3.jpg" },
+    { img: "./img/popular/cb1_4.jpg" },
+    { img: "./img/popular/cb1_5.jpg" },
+  ];
+
+  const ref = useRef();
 
   return (
     <div className="popular">
       <h2 className="popular_h2">ПОПУЛЯРНЫЕ КНИГИ</h2>
       <div className="popular-cards">
-        {/* <img className="popular_ar" src="./img/caret_left.svg" alt="left" /> */}
-
-        <Swiper
-          onSwiper={(swiper) => {
-            swiperRef.current = swiper;
+        <Splide
+          className="popular-slider"
+          ref={ref}
+          tag="section"
+          aria-label="Popular"
+          hasTrack={false}
+          options={{
+            arrows: false,
+            pagination: false,
+            perPage: 4,
+            focus: 0,
+            omitEnd: true,
+            gap: 30,
+            breakpoints: {
+              460: {
+                gap: 10,
+                perPage: 1,
+              },
+              600: {
+                gap: 10,
+                perPage: 1,
+              },
+              900: {
+                gap: 10,
+                perPage: 2,
+              },
+              1200: {
+                gap: 10,
+                perPage: 3,
+              },
+            },
           }}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            600: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            900: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            1200: {
-              slidesPerView: 4,
-              spaceBetween: 50,
-            },
-          }}
-          className="mySwiper"
         >
-          <SwiperSlide>
-            <div className="popular_card">
-              <img
-                className="popular_cover"
-                src="./img/popular/cb1_1.jpg"
-                alt="cover_book1"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="popular_card">
-              <img
-                className="popular_cover"
-                src="./img/popular/cb1_2.jpg"
-                alt="cover_book2"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="popular_card">
-              <img
-                className="popular_cover"
-                src="./img/popular/cb1_3.jpg"
-                alt="cover_book3"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="popular_card">
-              <img
-                className="popular_cover"
-                src="./img/popular/cb1_4.jpg"
-                alt="cover_book4"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="popular_card">
-              <img
-                className="popular_cover"
-                src="./img/popular/cb1_5.jpg"
-                alt="cover_book4"
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
+          <SplideTrack>
+            {popularItems.map((item, index) => (
+              <SplideSlide key={index}>
+                <div className="popular_card">
+                  <img
+                    className="popular_cover"
+                    src={`${item.img}`}
+                    alt="cover_book1"
+                  />
+                </div>
+              </SplideSlide>
+            ))}
+          </SplideTrack>
+          <div className="splide__arrows"></div>
+        </Splide>
         <button
-          className="popular-cards__slide-prev"
-          onClick={() => swiperRef.current.slidePrev()}
+          className="popular-slider__arrow popular-slider__arrow--prev"
+          onClick={() => ref.current.splide.go("<")}
         >
-          <img src="./img/caret_left.svg" alt="Previus slide" />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="CARET_LEFT">
+              <path
+                id="Vector"
+                d="M20 26L10 16L20 6"
+                stroke="#3D3C3C"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </g>
+          </svg>
         </button>
         <button
-          className="popular-cards__slide-next"
-          onClick={() => swiperRef.current.slideNext()}
+          className="popular-slider__arrow popular-slider__arrow--next"
+          onClick={() => ref.current.splide.go(">")}
         >
-          <img src="./img/caret_right.svg" alt="Next slide" />
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="CARET_RIGHT">
+              <path
+                id="Vector"
+                d="M12 6L22 16L12 26"
+                stroke="#3D3C3C"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </g>
+          </svg>
         </button>
-        {/* <img className="popular_ar" src="./img/caret_right.svg" alt="right" /> */}
       </div>
     </div>
   );
