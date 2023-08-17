b55d936d382c7b0c4be89866476f84ec865eeb22
FreshBlood1121 <perviburger@gmail.com>
2023-08-17 20:09:40 +0300

    created a relationship (many to many) between the tables of categories and books, changed the routing, fixed the bug

--- api/app/Models/BookHasCategory.php
+++ api/app/Models/BookHasCategory.php
@@ -5,23 +5,16 @@ namespace App\Models;
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Category extends Model
+class BookHasCategory extends Model
 {
     use Uuids;
     use HasFactory;
 
+    protected $table = 'books_has_categories';
+
     protected $fillable = [
-        'title',
-        'description',
         'created_at',
         'updated_at',
     ];
-
-    public function books(): BelongsToMany
-    {
-        return $this->belongsToMany(Book::class, 'books_has_categories');
-    }
-
 }
